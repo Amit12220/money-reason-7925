1ffd9db23d03f2f0925c03b0d1a321c771e1826c
Amit12220 <ajha.9614938@gmail.com>
2022-11-13 12:39:53 +0530

    project CMIS

--- CIMS_1/src/com/Utility/DBUtil.java
+++ CIMS_1/src/com/Utility/DBUtil.java
@@ -3,33 +3,54 @@ package com.Utility;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.ResourceBundle;
 
 public class DBUtil {
-
-public static Connection getConnection() {
-		
-		Connection conn=null;
-		
-		
-		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			
-		} catch (ClassNotFoundException e) {
-			
-			e.printStackTrace();
-		}
-		
-		String url="jdbc:mysql://localhost:3306/criminal";
-		
-		try {
-			
-			conn=DriverManager.getConnection(url,"root","Amit@2655");
-			
-		} catch (SQLException e) {
-	
-			e.printStackTrace();
-		}
-		
-		return conn;
-	}
+    
+    
+    private static String url;
+    private static String driverName;
+    private static String username;
+    private static String password;
+    
+    static {
+        
+        ResourceBundle rb= ResourceBundle.getBundle("dbdetails");
+        
+        url= rb.getString("db.url");
+        driverName= rb.getString("db.drivername");
+        username= rb.getString("db.username");
+        password= rb.getString("db.password");
+        
+        
+        
+    }
+    
+    
+    
+    public static Connection getConnection() {
+        
+        Connection conn = null;
+        
+        
+        try {
+            Class.forName(driverName);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        
+        
+        
+        try {
+            conn = DriverManager.getConnection(url, username, password);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        
+        
+        
+        
+        return conn;
+    }
 }
+
